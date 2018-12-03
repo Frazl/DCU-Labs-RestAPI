3a4d83ff4fab1e4b21c339d1361d89282785eeb9
Frazl <sean.fradl2@mail.dcu.ie>
2018-12-03 16:57:41 +0000

    init

---
+++ check.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests
+import warnings
+import datetime
+import json
+
+def check_lab(WEEK, DAY, ROOM):
+    page = requests.get("https://www101.dcu.ie/timetables/feed.php?room=GLA."+ROOM+"&week="+ROOM+"&hour=1-20&day="+DAY+"&hour=1-20&template=location", verify=False)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    p = soup.find_all("td")
+    x = []
+    s = ""
+    for i in range(56, len(p)):
+        lecture = str(p[i]).find('colspan="')
+        free = str(p[i]).find('src="../images/space.gif"')
+        if free != -1:
+            x.append('FREE')
+        elif lecture != -1:
+            for i in range(int(str(p[i])[lecture+9])):
+                x.append('NOT FREE')
+
+    times = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00', '17:30']
+    d = {}
+    for i in range(len(x)):
+        d[times[i]] = x[i]
+    return d 
+
+def DW():
+    n = datetime.datetime.now()
+    DAY = datetime.datetime.weekday(n) + 1 
+    WEEK = n.isocalendar()[1] - 38
+    return str(WEEK), str(DAY)
+
+
+def nexttime():
+    now = datetime.datetime.now()
+    hour = int(now.strftime("%H"))
+    minute = int(now.strftime("%M"))
+    if hour < 8 or hour >= 18 or (hour == 17 and minute > 29):
+        hour = "08:00"
+        return hour
+    else:
+        if minute >= 30:
+            minute = '00'
+            hour += 1
+        else:
+            minute = 30
+        if(len(str(hour))) == 1:
+            hour = '0' + str(hour)
+        s = str(hour)+":"+str(minute)
+        return s
+
+def consecutive(NEXT, all_labs, times):
+    consec = []
+    i = times.index(str(NEXT))
+    for lab in all_labs:
+        if lab[0][NEXT] == 'FREE':
+            consec.append(lab)
+    n_consec = []
+    while i < len(times) - 1:
+        n_consec = []
+        for lab in consec:
+            if lab[0][times[i]] == 'FREE':
+                n_consec.append(lab)
+        if len(n_consec) == 0:
+            return consec, NEXT, times[i]
+        consec = n_consec
+        i += 1 
+    return n_consec, NEXT, times[i]
+
+def main():
+    WEEK, DAY = DW() 
+    labs = ['LG25', 'LG26', 'L101', 'L114', 'L125', 'L128']
+    times = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00', '17:30']
+    NEXT = nexttime()
+    all_labs = []
+    with open('freelabs.txt', 'w') as f:
+        f.write(NEXT+'\n')
+        for lab in labs:
+            d = check_lab(WEEK, DAY, lab)
+            all_labs.append((d, lab))
+            if d[NEXT] == 'FREE':
+                f.write(lab+'\n')
+    with open('conseclabs.txt', 'w') as f:
+        consec, start, end = consecutive(NEXT, all_labs, times)
+        f.write(start+'\n')
+        f.write(end+'\n')
+        for lab in consec:
+            f.write(lab[1]+'\n')
+
+if __name__ == "__main__":
+    main()
+    print('FINISHED')
---
+++ index.js
@@ -0,0 +1,115 @@
+fs = require('fs')
+express = require('express')
+
+const spawn = require("child_process").exec;
+
+let g_labs = ['LG25', 'LG26', 'LG27', 'L101', 'L114', 'L125', 'L128',]
+let g_data = {}
+
+let update = (res) => {
+    proc = spawn('python3 ./get.py')
+    
+    proc.stdout.on('data', (data) => {
+        g_data = JSON.parse(data)
+        g_data.lastUpdate = new Date()
+        }
+    )
+
+    proc.on('exit', () => {
+        console.log('Finished Updating')
+        res()
+        }
+    )
+}
+
+function getTime(){
+    let hour = new Date().getHours()
+    let minute = new Date().getMinutes()
+
+    if(minute > 30){
+        minute = '00'
+        hour += 1
+    }
+    else{
+        minute = '30'
+    }
+    if(String(hour).length == 1){
+        hour = '0'+String(hour)
+    }
+    hour = String(hour)
+    return hour+':'+minute
+}
+
+function consecutive(exclude=undefined) {
+    let labs = g_labs
+    let data = g_data
+    let result = {}
+
+    if(exclude !== undefined){
+        for(let i = 0; i < exclude.length; i++){
+            let j = labs.indexOf(exclude[i])
+            labs.splice(j, 1)
+        }
+    }
+    const times = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00', '17:30']
+    
+    let nowIndex = times.indexOf(getTime())
+    if (nowIndex === -1){
+        nowIndex = 0
+    }
+    let avail = []
+    for(let i = nowIndex; i < times.length; i++){
+        for(labIndex in labs){
+            if(data[labs[labIndex]][times[i]] == 'FREE'){
+                avail.push(labs[labIndex])
+            }
+        }
+        if(avail.length == 0){
+            result.labs = labs
+            result.freeUntill = times[i]
+            return result
+        }
+        labs = avail
+        avail = []
+        nowIndex = i
+
+
+    }
+    result.labs = labs
+    result.freeUntill = times[nowIndex]
+    return result
+
+}
+
+var app = express()
+
+app.get('/api/v1/max', (req, res) => {
+    let result = consecutive([req.query.exclude])
+    res.status(200).send(result)
+})
+
+app.get('/api/v1/labs', (req, res) => {
+    res.status(200).send(g_data)
+})
+
+app.get('/api/v1/lab', (req, res) => {
+    res.status(200).send(g_data[req.query.lab])
+})
+
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+  });
+
+function run() {
+    new Promise(update).then(() => { 
+        let n = consecutive()
+        console.log(n)
+    })
+}
+
+setInterval(run, 1000 * 60 * 10);
+
+run()
